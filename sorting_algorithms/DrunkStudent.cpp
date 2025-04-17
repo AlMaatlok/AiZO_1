1963184fc0b6d447062aa8bbf1855f48f1b19fb7
Alicja Matłok <163751160+AlMaatlok@users.noreply.github.com>
2025-04-17 22:44:36 +0200

    drunk student 2

--- sorting_algorithms/DrunkStudent.cpp
+++ sorting_algorithms/DrunkStudent.cpp
@@ -1,21 +1,43 @@
 #include "DrunkStudent.h"
-#include "../utils/Timer.h"
+
+#include <fstream>
+#include <iostream>
+#include <cstring>
+
 #include "../utils/FileHandler.h"
 #include "../utils/NumberGenerator.h"
-#include "../sorting_algorithms/Sort.h"
-
-#include <chrono>
-#include <cstdlib>
-#include <iostream>
-#include <ctime>
-#include <cstring>
-#include <fstream>
-
-using namespace std;
-using namespace chrono;
+#include "../utils/Timer.h"
+#include "Sort.h"
 
 template<typename T>
-DrunkStudent<T>::DrunkStudent(){};
+DrunkStudent<T>::DrunkStudent() {
+}
+
+template<typename T>
+T* DrunkStudent<T>::sort(T* data, int size) {
+    // build min-heap
+    for (int i = size / 2 - 1; i >= 0; i--)
+        heapify(data, size, i);
+
+    // extracting element from the heap
+    for (int i = size - 1; i >= 0; i--) {
+        // moving the current root to the last element of the array.
+        T temp = data[i];
+        data[i] = data[0];
+        data[0] = temp;
+
+        // calling max heapify on the shrink heap
+        heapify(data, i, 0);
+    }
+
+    Sort<T> sort;
+    bool isSorted = sort.is_sorted(data, size);
+    if (!isSorted) {
+        reverseArray(data, size);
+    }
+
+    return data;
+}
 
 template<typename T>
 void DrunkStudent<T>::sorting_test(int iterations, int size, int distribution) {
@@ -32,11 +54,10 @@ void DrunkStudent<T>::sorting_test(int iterations, int size, int distribution) {
         for(int j = 0; j < size; j++) {
             data[j] = number_generator.generate();
         }
-
         sort.sort_array_for_test2(data, size, distribution);
 
         timer.start();
-        T* sorted_data = this->sort(data, 0, size - 1);
+        T* sorted_data = this->sort(data, size);
         timer.stop();
         bool is_sorted = sort.is_sorted(sorted_data, size);
 
@@ -57,6 +78,7 @@ void DrunkStudent<T>::sorting_test(int iterations, int size, int distribution) {
     }
     delete[] time_file;
 }
+
 template<typename T>
 void DrunkStudent<T>::sorting_file(char* filename, int distribution) {
     FileHandler<T> file_handler;
@@ -69,20 +91,19 @@ void DrunkStudent<T>::sorting_file(char* filename, int distribution) {
     char* time_file = generate_time_results_filename(size, distribution);
     ofstream plik(time_file);
 
-    sort.sort_array_for_test2(data, size ,distribution);
+    sort.sort_array_for_test2(data, size, distribution);
 
     timer.start();
-    T* sorted_data = this->sort(data, 0, size - 1);
+    T* sorted_data = this->sort(data, size);
     timer.stop();
     bool is_sorted = sort.is_sorted(sorted_data, size);
     if (!is_sorted) {
         cout << "Sorting failed!" << endl;
     }
     else {
-        cout << "Sorting successful!" << endl;
+        cout << "Sorting successful";
         int time_result = timer.result();
         plik << time_result << endl;
-
         char* filename_sorted = generate_filename(size, distribution);
         file_handler.writeData(filename_sorted, size, sorted_data);
         delete[] filename_sorted;
@@ -91,52 +112,6 @@ void DrunkStudent<T>::sorting_file(char* filename, int distribution) {
     delete[] time_file;
 }
 
-template<typename T>
-int DrunkStudent<T>::partition(T* data, int left, int right) {
-    int pivotIndex = rand() % (right - left + 1) + left;
-    T pivot = data[pivotIndex];
-    std::swap(data[pivotIndex], data[right]);
-
-    int i = left - 1;
-    for (int j = left; j < right; j++) {
-        if (data[j] <= pivot) {
-            i++;
-            std::swap(data[i], data[j]);
-        }
-    }
-
-    if (rand() % 3 == 0) {
-        int randomIndex1 = rand() % (right - left + 1) + left;
-        int randomIndex2 = rand() % (right - left + 1) + left;
-        std::swap(data[randomIndex1], data[randomIndex2]);
-    }
-
-    swap(&data[i + 1], &data[right]);
-    return i + 1;
-}
-
-template<typename T>
-T* DrunkStudent<T>::sort(T* data, int left, int right) {
-    if (left < right) {
-        int p = partition(data, left, right);
-
-        if (rand() % 2 == 0) {
-            sort(data, left, p - 1);
-            sort(data, p + 1, right);
-        } else {
-            sort(data, left, p - 1);
-        }
-    }
-    return data;
-}
-
-
-template<typename T>
-void DrunkStudent<T>::swap(T *a, T *b) {
-    T temp = *a;
-    *a = *b;
-    *b = temp;
-}
 
 template <typename T>
 char* DrunkStudent<T>::generate_time_results_filename(int size, int distribution) {
@@ -165,7 +140,7 @@ char* DrunkStudent<T>::generate_time_results_filename(int size, int distribution
 }
 template <typename T>
 char* DrunkStudent<T>::generate_filename(int size, int distribution) {
-    const auto now = system_clock::now();
+    auto now = system_clock::now();
     time_t timeNow = system_clock::to_time_t(now);
 
     tm* timeInfo = localtime(&timeNow);
@@ -187,6 +162,45 @@ char* DrunkStudent<T>::generate_filename(int size, int distribution) {
     strcpy(result, s.c_str());
 
     return result;
+}
+
+template<typename T>
+void DrunkStudent<T>::heapify(T *data, int size, int i) {
+    int smallest = i;
+    int l = 2 * i + 1;
+    int r = 2 * i + 2;
+
+    // If the left child is larger than the root
+    if (l < size && data[l] < data[smallest])
+        smallest = l;
+
+    // If the right child is larger
+    if (r < size && data[r] < data[smallest])
+        smallest = r;
+
+    // If the root is not the largest
+    if (smallest != i) {
+        swap(&data[i], &data[smallest]);
+
+        // Heapifying the sub-tree repeatedly
+        heapify(data, size, smallest);
+    }
+}
+
+template<typename T>
+void DrunkStudent<T>::swap(T* a, T* b) {
+    T temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+template<typename T>
+void DrunkStudent<T>::reverseArray(T* data, int size) {
+    for (int i = 0; i < size / 2; i++) {
+        T temp = data[i];
+        data[i] = data[size - i - 1];
+        data[size - i - 1] = temp;
+    }
 }
 
 template class DrunkStudent<int>;
--- sorting_algorithms/DrunkStudent.h
+++ sorting_algorithms/DrunkStudent.h
@@ -6,15 +6,16 @@ template<typename T>
 class DrunkStudent {
 public:
     DrunkStudent();
-    T* sort(T* data, int left, int right);
+    T* sort(T* data, int size);
     void sorting_file(char* filename, int distribution);
     void sorting_test(int iterations, int size, int distribution);
 
 private:
-    int partition(T* data, int left, int right);
     char* generate_time_results_filename(int size, int distribution);
     char* generate_filename(int size, int distribution);
+    void heapify(T* data, int size, int i);
     void swap(T* a, T* b);
+    void reverseArray(T* data, int size);
 };
 
 
